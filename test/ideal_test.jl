07188b83d837390eb0cfcf79a1870fd6965a27dc
Onuki <onuki@mist.i.u-tokyo.ac.jp>
2024-02-25 19:55:39 +0900

    fix ideal test

--- test/ideal_test.jl
+++ test/ideal_test.jl
@@ -1,38 +1,37 @@
 using Nemo
 import KaniSQIsign
-import KaniSQIsign.Level1: QOrderElem, norm, LeftIdeal, FullRepresentInteger, two_e_good_element
 
 e1 = KaniSQIsign.Level1.ExponentForIsogeny
 e2 = KaniSQIsign.Level1.ExponentForTorsion
 f = KaniSQIsign.Level1.Cofactor
 
-function check_short_element()
+function check_short_element(param::Module)
     N = 101 * f * 3 * 5
     e = Int(ceil(log(2, p))) + e1
 
-    a = QOrderElem(1)
-    while norm(a) % (BigInt(2)^e1) != 0
-        a, found = FullRepresentInteger(N*BigInt(2)^e)
+    a = param.QOrderElem(1)
+    while param.norm(a) % (BigInt(2)^e1) != 0
+        a, found = param.FullRepresentInteger(N*BigInt(2)^e)
         if found
-            while gcd(a) != 1
-                a = div(a, gcd(a))
+            while param.gcd(a) != 1
+                a = div(a, param.gcd(a))
             end
         end
     end
 
-    I = LeftIdeal(a, BigInt(2)^e1 * 3 * 5 * f)
+    I = param.LeftIdeal(a, BigInt(2)^e1 * 3 * 5 * f)
     @assert norm(I) == BigInt(2)^e1 * 3 * 5 * f
 
-    x, a, b, found = two_e_good_element(I, e2)
+    x, a, b, found = param.two_e_good_element(I, e2)
     if found
-        @assert norm(x) % norm(I) == 0
-        @assert a^2 + b^2 == BigInt(2)^e2 - div(norm(x), norm(I))
+        @assert param.norm(x) % norm(I) == 0
+        @assert a^2 + b^2 == BigInt(2)^e2 - div(param.norm(x), param.norm(I))
     else
         println("Not found")
     end
 end
 
 for _ in 1:1
-    check_short_element()
+    check_short_element(KaniSQIsign.Level1)
 end
 println("Short ideal tests passed!")
